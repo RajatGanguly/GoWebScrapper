242318086b297282031030391ea46c1032648b6b
RAJAT <admin@selflerner.org>
2022-10-30 22:14:07 +0530

    Web request handling has been added

--- main.go
+++ main.go
@@ -1,5 +1,29 @@
 package main
-import "fmt"
-func main(){
-	fmt.Println("Go Web Scrapper")
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
+
+const url = "https://amazon.com"
+
+func main() {
+	fmt.Println("WEB REQUEST")
+	response, err := http.Get(url)
+	if err != nil {
+		fmt.Println("PANIC Happens")
+		panic(err)
+	}
+	fmt.Printf("%T\n", response)
+
+	defer response.Body.Close()
+	dataBytes, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("PANIC Happens")
+		panic(err)
+	}
+	content := string(dataBytes)
+	fmt.Println(content)
 }
+
